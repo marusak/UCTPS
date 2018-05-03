38c5de6ed0a95f6c02b72b1ef55a83710578f984
Matej Marusak <mmarusak@redhat.com>
2018-05-03 12:42:39 +0200

    Print only each 100th generation
    Signed-off-by: Matej Marusak <mmarusak@redhat.com>

--- uctps.c
+++ uctps.c
@@ -41,14 +41,20 @@ int main(int argc, char **argv) {
     }
 
     int penalty = 100;
-    int remaining_generations = 3000;
+    int remaining_generations = 250;
+    int print_n = 0;
     while(penalty != 0 && remaining_generations > 0){
         penalty = generation(tts, N, p);
-        printf("%d\n",penalty);
+        if (print_n >=100){
+            printf("%d %d\n",250000-remaining_generations, penalty);
+            print_n = 0;
+        } else
+            print_n++;
         remaining_generations--;
     }
 
     timetable_t* tt = best_timetable(tts, N, p);
+    printf("%d %d\n",250000, penalty);
     print_timetable(tt);
 
     for(int i = 0; i < N; i++)
